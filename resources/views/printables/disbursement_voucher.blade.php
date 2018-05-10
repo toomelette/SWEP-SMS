76a89809ebe514b6680ba4bb1a307be01fa3c631
toomelette <toomelette@gmail.com>
2018-05-10 20:05:01 +0800

    Disb. Voucher Print Front Border Fix

--- resources/views/printables/disbursement_voucher.blade.php
+++ resources/views/printables/disbursement_voucher.blade.php
@@ -64,9 +64,9 @@
 
 
       {{-- DOCUMENT NAME --}}
-      <div class="row" style="border-top:solid;">
+      <div class="row" style="border-top:solid 1.4px;">
         
-        <div class="col-sm-9" style="border-right:solid; text-align: center; padding-top:9px; padding-bottom:9px;"> 
+        <div class="col-sm-9" style="border-right:solid 1.4px; text-align: center; padding-top:9px; padding-bottom:9px;"> 
           <span style="font-weight:bold; font-size:20px;">DISBURSEMENT VOUCHER</span>
         </div>
 
@@ -79,9 +79,9 @@
 
 
       {{-- MODE OF PAYMENT --}}
-      <div class="row" style="border-top:solid;">
+      <div class="row" style="border-top:solid 1.4px;">
         
-        <div class="col-sm-2 div-height" style="border-right:solid; line-height: 1; text-align: center;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px; line-height: 1; text-align: center;"> 
           <span style="font-size:12px; font-weight:bold; white-space: nowrap;">Mode of</span>
           <br><span style="font-size:12px; font-weight:bold;">Payment</span>
         </div>
@@ -92,7 +92,7 @@
               @if($data->mode_of_payment_id == $disbursement_voucher->mode_of_payment)
                 <div style="width: 20px; height: 20px; border: 10px solid;"></div>
               @else
-                <div style="width: 20px; height: 20px; border: 3px solid;"></div>
+                <div style="width: 20px; height: 20px; border: 1.4px solid;"></div>
               @endif
             </div>
             <div class="col-sm-1" style="padding-left: 20px;">
@@ -105,17 +105,17 @@
 
 
       {{-- PAYEE --}}
-      <div class="row" style="border-top:solid; overflow:hidden;">
+      <div class="row" style="border-top:solid 1.4px; overflow:hidden;">
         
-        <div class="col-sm-2 div-height" style="border-right:solid;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px;"> 
           <p style="font-size:12px; padding-top:10px; padding-bottom:5px; padding-left: 3px; font-weight:bold;">Payee:</p>
         </div>
 
-        <div class="col-sm-4 div-height" style="border-right:solid; padding-left: 0;">
+        <div class="col-sm-4 div-height" style="border-right:solid 1.4px; padding-left: 0;">
           <span style="font-size:12px; font-weight:bold; word-wrap: break-word; margin-left: 2px;">{!! $disbursement_voucher->payee !!}</span>
         </div>
 
-        <div class="col-sm-3 div-height" style="border-right:solid; line-height: 1.3; padding-left:0;">
+        <div class="col-sm-3 div-height" style="border-right:solid 1.4px; line-height: 1.3; padding-left:0;">
           <span style="font-size:9px; padding-left:2px;">TIN / Employee No.:</span>
           <br><span style="font-size:12px; font-weight:bold; padding-left:15px;">{!! $disbursement_voucher->tin !!}</span>
         </div>
@@ -129,24 +129,24 @@
 
 
       {{-- ADDRESS --}}
-      <div class="row" style="border-top:solid; overflow:hidden;">
+      <div class="row" style="border-top:solid 1.4px; overflow:hidden;">
         
-        <div class="col-sm-2 div-height" style="border-right:solid;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px;"> 
           <p style="font-size:12px; padding-top:10px; padding-bottom:5px; padding-left: 3px; font-weight:bold;">Address:</p>
         </div>
 
-        <div class="col-sm-4 div-height" style="border-right:solid; padding-left: 0;">
+        <div class="col-sm-4 div-height" style="border-right:solid 1.4px; padding-left: 0;">
           <span style="font-size:12px; font-weight:bold; word-wrap: break-word; margin-left: 2px;">{!! $disbursement_voucher->address !!}</span>
         </div>
 
 
         <div class="col-sm-6" style="padding:0%; margin-top:-6px; overflow:hidden;">
           
-          <div class="col-sm-12" style="border-bottom:solid; width: 100%; text-align: center;">
+          <div class="col-sm-12" style="border-bottom:solid 1.4px; width: 100%; text-align: center;">
             <span style="font-size:10px; font-weight:bold;">Resposibility Center</span>
           </div>
 
-          <div class="col-sm-6" style="border-right:solid; padding-left:0; line-height:1.3;">
+          <div class="col-sm-6" style="border-right:solid 1.4px; padding-left:0; line-height:1.3;">
             <span style="font-size:9px; padding-left:2px;">Office/Unit/Project:</span>
             <br><span style="font-size:12px; font-weight:bold; padding-left:15px;">{!! $disbursement_voucher->department_unit_name !!}</span>
           </div>
@@ -162,9 +162,9 @@
 
 
       {{-- EXPLANATION --}}
-      <div class="row" style="border-top:solid;">
+      <div class="row" style="border-top:solid 1.4px;">
         
-        <div class="col-sm-9" style="border-right:solid; text-align: center; padding-top:2px; padding-bottom:2px;"> 
+        <div class="col-sm-9" style="border-right:solid 1.4px; text-align: center; padding-top:2px; padding-bottom:2px;"> 
           <span style="font-weight:bold; font-size:15px;">EXPLANATION</span>
         </div>
 
@@ -176,13 +176,13 @@
 
 
       {{-- EXPLANATION VALUE --}}
-      <div class="row" style="border-top:solid; height:20em; overflow: hidden;">
+      <div class="row" style="border-top:solid 1.4px; height:23em; overflow: hidden;">
         
-        <div class="col-sm-9" style="border-right:solid; padding-left:20px; margin-bottom: -250px; padding-bottom: 250px;"> 
+        <div class="col-sm-9" style="border-right:solid 1.4px; padding-left:20px; margin-bottom: -300px; padding-bottom: 300px;"> 
           <p style="font-family:Arial; font-size:16px; white-space: pre-wrap; white-space: -moz-pre-wrap;  white-space: -pre-wrap; white-space: -o-pre-wrap; word-wrap: break-word;">{!! $disbursement_voucher->explanation !!}</p>
         </div>
 
-        <div class="col-sm-3" style="border-right:solid; text-align: center; padding-top: 20px;"> 
+        <div class="col-sm-3" style="border-right:solid 1.4px; text-align: center; padding-top: 20px;"> 
           <span style="font-weight:bold; font-size:15px;">{{ number_format($disbursement_voucher->amount, 2) }}</span>
         </div>
 
@@ -190,16 +190,16 @@
 
 
       {{-- CERT & APPR HEAD --}}
-      <div class="row" style="border-top:solid;">
+      <div class="row" style="border-top:solid 1.4px;">
         
-        <div class="col-sm-6" style="border-right:solid;"> 
-          <span style="border-right: solid; border-bottom: solid; padding:4px; font-weight:bold;">A</span>
+        <div class="col-sm-6" style="border-right:solid 1.4px;"> 
+          <span style="border-right: solid 1.4px; border-bottom: solid 1.4px; padding:4px; font-weight:bold;">A</span>
           <p style="margin-left:40px; margin-top:-10px; font-weight:bold;">Certified</p>
           <p style="margin-left:100px; margin-top:-7px;">Supporting documents complete</p>
         </div>
 
-        <div class="col-sm-6 div-height" style="border-right:solid; padding-left: 0;"> 
-          <span style="border-right: solid; border-bottom: solid; padding:4px; font-weight:bold;">B</span>
+        <div class="col-sm-6 div-height" style="border-right:solid 1.4px; padding-left: 0;"> 
+          <span style="border-right: solid 1.4px; border-bottom: solid 1.4px; padding:4px; font-weight:bold;">B</span>
           <p style="margin-left:40px; margin-top:-10px; font-weight:bold;">Approved for Payment</p>
           <p style="margin-left:200px; margin-top:-7px; font-weight:bold;">{{ number_format($disbursement_voucher->amount, 2) }}</p>
         </div>
@@ -208,21 +208,21 @@
 
 
       {{-- SIGNATURES --}}
-      <div class="row" style="border-top:solid;">
+      <div class="row" style="border-top:solid 1.4px; margin-top: -2px; margin-bottom:5px;">
         
-        <div class="col-sm-2 div-height" style="border-right:solid;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px;"> 
           <span style="font-size:12px;">&nbsp;Signature:</span>
         </div>
 
-        <div class="col-sm-4 div-height" style="border-right:solid;">
+        <div class="col-sm-4 div-height" style="border-right:solid 1.4px;">
           &nbsp;
         </div>
 
-        <div class="col-sm-2 div-height" style="border-right:solid; padding-left:0;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px; padding-left:0;"> 
           <span style="font-size:12px;">&nbsp;Signature:</span>
         </div>
 
-        <div class="col-sm-4 div-height" style="border-right:solid;"> 
+        <div class="col-sm-4 div-height" style="border-right:solid 1.4px;"> 
           &nbsp;
         </div>
 
@@ -230,13 +230,13 @@
 
 
       {{-- NAMES --}}
-      <div class="row" style="border-top:solid;">
+      <div class="row" style="border-top:solid 1.4px;">
         
-        <div class="col-sm-2 div-height" style="border-right:solid;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px;"> 
           <span style="font-size:12px;">&nbsp;Printed Name:</span>
         </div>
 
-        <div class="col-sm-4 div-height" style="border-right:solid;">
+        <div class="col-sm-4 div-height" style="border-right:solid 1.4px;">
           @foreach($global_signatories_all as $data)
             @if($data->type == 2)
               <span style="font-size:12px; font-weight:bold;">&nbsp;{{ $data->employee_name }}</span>
@@ -244,11 +244,11 @@
           @endforeach
         </div>
 
-        <div class="col-sm-2 div-height" style="border-right:solid; padding-left:0;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px; padding-left:0;"> 
           <span style="font-size:12px;">&nbsp;Printed Name:</span>
         </div>
 
-        <div class="col-sm-4 div-height" style="border-right:solid;"> 
+        <div class="col-sm-4 div-height" style="border-right:solid 1.4px;"> 
           @foreach($global_signatories_all as $data)
             @if($data->type == 1)
               <span style="font-size:12px; font-weight:bold;">&nbsp;{{ $data->employee_name }}</span>
@@ -260,15 +260,15 @@
 
 
       {{-- POSITION --}}
-      <div class="row" style="border-top:solid;">
+      <div class="row" style="border-top:solid 1.4px; overflow: hidden;">
         
-        <div class="col-sm-2 div-height" style="border-right:solid;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px;"> 
           <span style="font-size:12px;">&nbsp;Position:</span>
         </div>
 
-        <div class="col-sm-4" style="border-right:solid; padding:0%; margin-top:-6px; overflow:hidden;">
+        <div class="col-sm-4" style="border-right:solid 1.4px; padding:0%; margin-top:-6px; overflow:hidden;">
 
-          <div class="col-sm-12" style="border-bottom:solid; width: 100%; padding-top:5px; padding-bottom:5px;">
+          <div class="col-sm-12" style="border-bottom:solid 1.4px; width: 100%; padding-top:5px; padding-bottom:5px;">
             @foreach($global_signatories_all as $data)
               @if($data->type == 2)
                 <span style="font-size:12px; font-weight:bold;">&nbsp;{{ $data->employee_position }}</span>
@@ -282,13 +282,13 @@
 
         </div>
 
-        <div class="col-sm-2 div-height" style="border-right:solid; padding-left:0;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px; padding-left:0;"> 
           <span style="font-size:12px;">&nbsp;Position:</span>
         </div>
 
-        <div class="col-sm-4" style="border-right:solid; padding:0%; margin-top:-6px; overflow:hidden;">
+        <div class="col-sm-4" style="border-right:solid 1.4px; padding:0%; margin-top:-6px; overflow:hidden;">
 
-          <div class="col-sm-12" style="border-bottom:solid; width: 100%; padding-top:5px; padding-bottom:5px;">
+          <div class="col-sm-12" style="border-bottom:solid 1.4px; width: 100%; padding-top:5px; padding-bottom:5px;">
             @foreach($global_signatories_all as $data)
               @if($data->type == 1)
                 <span style="font-size:12px; font-weight:bold;">&nbsp;{{ $data->employee_position }}</span>
@@ -306,21 +306,21 @@
 
 
       {{-- DATE --}}
-      <div class="row" style="border-top:solid;">
+      <div class="row" style="border-top:solid 1.4px;">
         
-        <div class="col-sm-2" style="border-right:solid;"> 
+        <div class="col-sm-2" style="border-right:solid 1.4px; overflow: hidden"> 
           <span style="font-size:12px;">&nbsp;Date:</span>
         </div>
 
-        <div class="col-sm-4" style="border-right:solid;">
+        <div class="col-sm-4" style="border-right:solid 1.4px;">
           &nbsp;
         </div>
 
-        <div class="col-sm-2" style="border-right:solid; padding-left:0;"> 
+        <div class="col-sm-2" style="border-right:solid 1.4px; padding-left:0;"> 
           <span style="font-size:12px;">&nbsp;Date:</span>
         </div>
 
-        <div class="col-sm-4" style="border-right:solid;">
+        <div class="col-sm-4" style="border-right:solid 1.4px;">
           &nbsp;
         </div>
 
@@ -328,14 +328,14 @@
 
 
       {{-- RECIEVED PAYMENT --}}
-      <div class="row" style="border-top:solid;">
+      <div class="row" style="border-top:solid 1.4px;">
         
-        <div class="col-sm-8" style="border-right:solid;"> 
-          <span style="border-right: solid; border-bottom: solid; padding-top:4px; padding-right:4px; padding-left:4px; padding-bottom:2px; font-weight:bold;">C</span>
+        <div class="col-sm-8" style="border-right:solid 1.4px;"> 
+          <span style="border-right: solid 1.4px; padding-top:4px; padding-right:4px; padding-left:4px; padding-bottom:2px; font-weight:bold;">C</span>
           <span style="font-weight:bold; font-weight:bold; margin-left:10px;">Received Payment:</span>
         </div>
 
-        <div class="col-sm-4" style="border-right:solid; padding-left:0;"> 
+        <div class="col-sm-4" style="border-right:solid 1.4px; padding-left:0;"> 
           <span style="font-size:13px; font-weight:bold;">&nbsp;JEV No.</span>
         </div>
 
@@ -345,19 +345,19 @@
       {{-- CHECK / ADA NO. --}}
       <div class="row" style="padding-bottom: 5px; overflow:hidden;">
         
-        <div class="col-sm-2 div-height" style="border-right:solid; border-top: solid;">
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px; border-top: solid 1.4px;">
           <p style="font-size:10px;">&nbsp;Check/ADA No.:</p>
         </div>
 
-        <div class="col-sm-2 div-height" style="border-right:solid; border-top: solid;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px; border-top: solid 1.4px;"> 
           &nbsp;
         </div>
 
-        <div class="col-sm-1 div-height" style="border-right:solid; border-top: solid; padding-left:0;"> 
+        <div class="col-sm-1 div-height" style="border-right:solid 1.4px; border-top: solid 1.4px; padding-left:0;"> 
          <p style="font-size:9px;">&nbsp;Date:</p>
         </div>
 
-        <div class="col-sm-3 div-height" style="border-right:solid; border-top: solid; padding-left:0;"> 
+        <div class="col-sm-3 div-height" style="border-right:solid 1.4px; border-top: solid 1.4px; padding-left:0;"> 
           <p style="font-size:9px;">&nbsp;Bank Name:</p>
         </div>
 
@@ -365,21 +365,21 @@
 
 
       {{-- SIGNATURE 2 --}}
-      <div class="row" style="padding-bottom: 5px; border-top:solid; overflow:hidden;">
+      <div class="row" style="padding-bottom: 5px; border-top:solid 1.4px; overflow:hidden;">
         
-        <div class="col-sm-2 div-height" style="border-right:solid;">
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px;">
           <p style="font-size:11px;">&nbsp;Signature:</p>
         </div>
 
-        <div class="col-sm-2 div-height" style="border-right:solid;"> 
+        <div class="col-sm-2 div-height" style="border-right:solid 1.4px;"> 
           &nbsp;
         </div>
 
-        <div class="col-sm-1 div-height" style="border-right:solid; padding-left:0;"> 
+        <div class="col-sm-1 div-height" style="border-right:solid 1.4px; padding-left:0;"> 
          <p style="font-size:9px;">&nbsp;Date:</p>
         </div>
 
-        <div class="col-sm-3 div-height" style="border-right:solid; padding-left:0;"> 
+        <div class="col-sm-3 div-height" style="border-right:solid 1.4px; padding-left:0;"> 
           <p style="font-size:9px;">&nbsp;Printed Name:</p>
         </div>
 
@@ -393,11 +393,11 @@
       {{-- OR --}}
       <div class="row" style="padding-bottom: 5px;">
         
-        <div class="col-sm-8 div-height" style="border-right:solid; border-top:solid;">
+        <div class="col-sm-8 div-height" style="border-right:solid 1.4px; border-top:solid 1.4px;">
           <p style="font-size:10px;">&nbsp;Official Receipt (OR)/Other Documents:</p>
         </div>
 
-        <div class="col-sm-4 div-height" style="border-right:solid; padding-left:0;"> 
+        <div class="col-sm-4 div-height" style="border-right:solid 1.4px; padding-left:0;"> 
           <span style="font-size:9px;">&nbsp;</span>
         </div>
 
@@ -413,7 +413,7 @@
 <div class="row">
   
   <div class="col-sm-8 div-height">
-    <p style="font-size:10px;">Username: {!! Auth::user()->username !!}</p>
+    <p style="font-size:10px;">Username: {!! Auth::user()->username !!} | Doc No: {{ $disbursement_voucher->doc_no }}</p>
   </div>
 
   <div class="col-sm-4 div-height" style="border-right:solid; padding-left:0; line-height: 1.2;"> 
