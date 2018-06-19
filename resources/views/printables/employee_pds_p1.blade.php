6290d393bdd0868cfb919e8583aee2f86b997483
toomelette <toomelette@gmail.com>
2018-06-19 15:35:59 +0800

    Employee with PDS Part 13

--- resources/views/printables/employee_pds_p1.blade.php
+++ resources/views/printables/employee_pds_p1.blade.php
@@ -39,6 +39,11 @@
 
     @media print{
 
+    	.col-pds-1 {
+        	width: 14.25%;
+        	float: left;
+   		}
+
     	.col-pds-2 {
         	width: 28.5%;
         	float: left;
@@ -76,14 +81,14 @@
     <div class="row">
       <div class="col-md-1"></div>
       <div class="col-md-10" style="text-align: center;">
-        <span style="font-weight:bold; font-size:30px;">PERSONAL DATA SHEET</span>
+        <span style="font-weight:bold; font-size:28px;">PERSONAL DATA SHEET</span>
       </div>
       <div class="col-md-1"></div>
     </div>
 
     <div class="row">
       <div class="col-md-12" style="margin-bottom: -9px;">
-        <p style="font-style: italic; font-weight:bold; font-size:8px;">WARNING: Any misrepresentation made in the Personal Data Sheet and the Work Experience Sheet shall cause the filing of administrative/criminal case/s against the person concerned.
+        <p style="font-style: italic; font-weight:bold; font-size:8px;">WARNING: Any misrepresentation made in the Personal Data Sheet and the Work Experience Sheet shall cause the filing of administrative/criminal case/s against the person concerned.<br>
           <br>READ THE ATTACHED GUIDE TO FILLING OUT THE PERSONAL DATA SHEET (PDS) BEFORE ACCOMPLISHING THE PDS FORM.
         </p>
       </div>
@@ -133,12 +138,11 @@
 
 
     <div class="row">
-      <div class="col-sm-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px;">
-        <span style="font-size:8px;">&nbsp;&nbsp;3. DATE OF BIRTH</span>
-        <span style="font-size:8px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(mm/dd/yyyy)</span>
+      <div class="col-sm-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px; padding-bottom: 10px;">
+        <p style="font-size:8px;">&nbsp;&nbsp;3. DATE OF BIRTH<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(mm/dd/yyyy)</p>
       </div>
-      <div class="col-sm-3" style="border-bottom:solid 1px;">&nbsp;</div>
-      <div class="col-sm-3 box-l-grey" style="border-left:solid 1px; border-right:solid 1px; border-bottom:solid 1px; border-bottom-color: #e8e8e8;">
+      <div class="col-sm-3" style="border-bottom:solid 1px; padding-bottom: 22px;">&nbsp;</div>
+      <div class="col-sm-3 box-l-grey" style="border-left:solid 1px; border-right:solid 1px; border-bottom:solid 1px; border-bottom-color: #e8e8e8; padding-bottom: 2px;">
         <span style="font-size:8px;">16. CITIZENSHIP</span>
         <br>&nbsp;
       </div>
@@ -177,12 +181,10 @@
     </div>
 
     <div class="row">
-      <div class="col-sm-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px; padding-bottom:9px;">
+      <div class="col-sm-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px; padding-bottom:49px;">
         <span style="font-size:8px;">&nbsp;&nbsp;6. CIVIL STATUS</span>
-        <br>&nbsp;
-        <br>&nbsp;
       </div>
-      <div class="col-sm-3" style="border-bottom:solid 1px; padding-bottom:10.2px;">
+      <div class="col-sm-3" style="border-bottom:solid 1px; padding-bottom:9px;">
         <span style="font-size:8px;">&#9745; SINGLE</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size:8px;">&#9745; MARRIED</span><br>
         <span style="font-size:8px;">&#9745; WIDOWED</span> &nbsp;&nbsp;&nbsp;<span style="font-size:8px;">&#9745; SEPERATED</span><br>
         <span style="font-size:8px;">&#9745; OTHER/S</span>
@@ -215,7 +217,7 @@
     </div>
 
     <div class="row">
-      <div class="col-sm-2 box-l-grey" style="border-right:solid 1px; padding-bottom:14px; border-bottom:solid 1px;">
+      <div class="col-sm-2 box-l-grey" style="border-right:solid 1px; padding-bottom:13px; border-bottom:solid 1px;">
         <span style="font-size:8px;">&nbsp;&nbsp;7. HEIGHT (m)</span>
       </div>
       <div class="col-sm-3" style="border-bottom:solid 1px; padding-bottom:13px;">
@@ -393,18 +395,18 @@
       </div>
     </div>
 
-    <div class="row" style="border-bottom:solid 1px;">
+    <div class="row">
 
       <div class="col-sm-7 no-padding" style="border-right:solid 1px;">
 
-      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px;">
+      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; padding-bottom: 1px;">
       		<span style="font-size:8px; margin-left:13px;">&nbsp;&nbsp;22. SPOUSE'S SURNAME</span>
       	</div>
       	<div class="col-pds-10" style="border-bottom:solid 1px;">
       		<span style="font-size:8px;">&nbsp;&nbsp;</span>
       	</div>
 
-      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px;">
+      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; padding-bottom: 1px;">
       		<span style="font-size:8px; margin-left:17px;">&nbsp;&nbsp;FIRSTNAME</span>
       	</div>
       	<div class="col-pds-8" style="border-bottom:solid 1px;"">
@@ -449,14 +451,14 @@
       		<span style="font-size:8px;">&nbsp;&nbsp;</span>
       	</div>
 
-      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px;">
+      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; padding-bottom: 1px;">
       		<span style="font-size:8px; margin-left:13px;">&nbsp;&nbsp;24. FATHER'S SURNAME</span>
       	</div>
       	<div class="col-pds-10" style="border-bottom:solid 1px;">
       		<span style="font-size:8px;">&nbsp;&nbsp;</span>
       	</div>
 
-      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px;">
+      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; padding-bottom: 1px;">
       		<span style="font-size:8px; margin-left:17px;">&nbsp;&nbsp;FIRSTNAME</span>
       	</div>
       	<div class="col-pds-8" style="border-bottom:solid 1px;"">
@@ -473,31 +475,24 @@
       		<span style="font-size:8px;">&nbsp;&nbsp;</span>
       	</div>
 
-      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px;">
+      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; padding-bottom: 1px;">
       		<span style="font-size:7px; margin-left:13px;">&nbsp;&nbsp;25. MOTHER'S MAIDEN NAME</span>
       	</div>
       	<div class="col-pds-10" style="border-bottom:solid 1px;">
       		<span style="font-size:8px;">&nbsp;&nbsp;</span>
       	</div>
 
-      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px;">
+      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; padding-bottom: 1px;">
       		<span style="font-size:8px; margin-left:17px;">&nbsp;&nbsp;SURNAME</span>
       	</div>
       	<div class="col-pds-10" style="border-bottom:solid 1px;"">
       		<span style="font-size:8px;">&nbsp;&nbsp;</span>
       	</div>
 
-      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px;">
+      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; padding-bottom: 1px;">
       		<span style="font-size:8px; margin-left:17px;">&nbsp;&nbsp;FIRSTNAME</span>
       	</div>
-      	<div class="col-pds-10" style="border-bottom:solid 1px;"">
-      		<span style="font-size:8px;">&nbsp;&nbsp;</span>
-      	</div>
-
-      	<div class="col-pds-2 box-l-grey" style="border-right:solid 1px; border-bottom:solid 1px;">
-      		<span style="font-size:8px; margin-left:15px;">&nbsp;&nbsp;MIDDLENAME</span>
-      	</div>
-      	<div class="col-pds-10" style="border-bottom:solid 1px;">
+      	<div class="col-pds-10">
       		<span style="font-size:8px;">&nbsp;&nbsp;</span>
       	</div>
 
@@ -505,17 +500,31 @@
 
 
       <div class="col-sm-5 no-padding">
-
         <div class="col-sm-7 box-l-grey no-padding" style="border-right:solid 1px; border-bottom:solid 1px;">
       		<span style="font-size:7px;">&nbsp;23. NAME of CHILDREN (Write full name and list all)</span>
       	</div>
       	<div class="col-sm-5 box-l-grey no-padding" style="border-bottom:solid 1px;">
       		<span style="font-size:7px;">&nbsp; DATE OF BIRTH (mm/dd/yyyy)</span>
       	</div>
-
       </div>
 
     </div>
+
+
+    <div class="row" style="border-bottom:solid 1px;">
+      	<div class="col-sm-2 box-l-grey no-padding" style="border-right:solid 1px; margin-left: -1px;">
+	  		<span style="font-size:8px; margin-left:17px;">&nbsp;&nbsp;&nbsp;MIDDLENAME</span>
+	  	</div>
+	  	<div class="col-sm-5" style="border-right:solid 1px; border-top:solid 1px; margin-left:1px;">
+	  		<span style="font-size:8px;">&nbsp;&nbsp;</span>
+	  	</div>
+	  	<div class="col-sm-5 box-l-grey" style="border-right:solid 1px; border-top:solid 1px; ">
+	  		<p style="font-size: 7px; text-align: center; color:red !important; font-style: italic; -webkit-print-color-adjust: exact; margin-top: 5px; margin-bottom: 5px;">
+	  			(Continue on a seperate sheet if necessary)
+	  		</p>
+	  	</div>
+    </div>
+
 
     {{-- EDUCATIONAL BACKGROUND --}}
     <div class="row box-d-grey" style="border-bottom:solid 2px;">
@@ -524,10 +533,228 @@
       </div>
     </div>
 
+    {{-- EB Header --}}
+    <div class="row" style="border-bottom:solid 1px;">
+      <div class="col-sm-2 box-l-grey" style="border-right:solid 1px;">
+      	<span style="font-size:8px;">26.</span>
+      	<span style="font-size:8px; padding-left:27px;"> LEVEL</span>
+      	<br>&nbsp;
+      </div>
+      <div class="col-sm-2 box-l-grey" style="border-right:solid 1px;">
+        <p style="font-size:8px; text-align: center; padding-top:7px;">NAME OF SCHOOL<br>(Write in full)</p>
+      </div>
+      <div class="col-sm-3 box-l-grey" style="border-right:solid 1px;">
+        <p style="font-size:8px; text-align: center; padding-top:7px;">BASIC EDUCATION/DEGREE/COURSE<br>(Write in full)</p>
+      </div>
+      <div class="col-sm-2 box-l-grey no-padding" style="border-right:solid 1px;">
+        <p style="font-size:7px; text-align: center; padding-top:7px;">PERIOD OF ATTENDANCE</p>
+        <div class="col-sm-12 no-padding" style="border-top: solid 1px; margin-bottom: -9px;">
+        	<div class="col-sm-6" style="border-right:solid 1px;">
+        		<p style="font-size:7px; text-align: center;">From</p>
+        	</div>
+        	<div class="col-sm-6">
+        		<p style="font-size:7px; text-align: center;">To</p>
+        	</div>
+        </div>
+      </div>
+      <div class="col-sm-1 box-l-grey no-padding" style="border-right:solid 1px;">
+        <p style="font-size:6px; text-align: center; padding-top: 5px;">HIGHEST LEVEL/<br>UNITS EARNED<br>(if not graduated)</p>
+      </div>
+      <div class="col-sm-1 box-l-grey no-padding" style="border-right:solid 1px;">
+        <p style="font-size:7px; text-align: center; padding-top: 9px;">YEAR<br>GRADUATED</p>
+      </div>
+      <div class="col-sm-1 box-l-grey" style="border-right:solid 1px; padding:0;">
+        <p style="font-size:6px; text-align: center; padding-right: 11px; margin-bottom:7px;">SCHOLARSHIP/<br>ACADEMIC<br>HONORS<br>RECEIVED</p>
+      </div>
+    </div>
+
+    {{-- EB Content --}}
+    <div class="row" style="border-bottom:solid 1px;">
+      <div class="col-sm-2 box-l-grey no-padding" style="border-right:solid 1px;">
+      	<span style="font-size:8px; padding-left:20px;">ELEMENTARY</span>
+      </div>
+      <div class="col-sm-2" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-3" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-2 no-padding" style="border-right:solid 1px;">
+        <div class="col-sm-12 no-padding">
+        	<div class="col-sm-6" style="border-right:solid 1px;">
+        		<span>&nbsp;</span>
+        	</div>
+        	<div class="col-sm-6">
+        		<span>&nbsp;</span>
+        	</div>
+        </div>
+      </div>
+      <div class="col-sm-1 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1" style="border-right:solid 1px; padding:0; margin-bottom: -20px;">
+        &nbsp;
+      </div>
+    </div>
+
+    <div class="row" style="border-bottom:solid 1px;">
+      <div class="col-sm-2 box-l-grey no-padding" style="border-right:solid 1px;">
+      	<span style="font-size:8px; padding-left:20px;">SECONDARY</span>
+      </div>
+      <div class="col-sm-2" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-3" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-2 no-padding" style="border-right:solid 1px;">
+        <div class="col-sm-12 no-padding">
+        	<div class="col-sm-6" style="border-right:solid 1px;">
+        		<span>&nbsp;</span>
+        	</div>
+        	<div class="col-sm-6">
+        		<span>&nbsp;</span>
+        	</div>
+        </div>
+      </div>
+      <div class="col-sm-1 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1" style="border-right:solid 1px; padding:0; margin-bottom: -20px;">
+        &nbsp;
+      </div>
+    </div>
+
+    <div class="row" style="border-bottom:solid 1px;">
+      <div class="col-sm-2 box-l-grey no-padding" style="border-right:solid 1px;">
+      	<span style="font-size:7px; padding-left:20px;">VOCATIONAL/TRADE COURSE</span>
+      </div>
+      <div class="col-sm-2" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-3" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-2 no-padding" style="border-right:solid 1px;">
+        <div class="col-sm-12 no-padding">
+        	<div class="col-sm-6" style="border-right:solid 1px;">
+        		<span>&nbsp;</span>
+        	</div>
+        	<div class="col-sm-6">
+        		<span>&nbsp;</span>
+        	</div>
+        </div>
+      </div>
+      <div class="col-sm-1 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1" style="border-right:solid 1px; padding:0; margin-bottom: -20px;">
+        &nbsp;
+      </div>
+    </div>
+
+    <div class="row" style="border-bottom:solid 1px;">
+      <div class="col-sm-2 box-l-grey no-padding" style="border-right:solid 1px;">
+      	<span style="font-size:8px; padding-left:20px;">COLLEGE</span>
+      </div>
+      <div class="col-sm-2" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-3" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-2 no-padding" style="border-right:solid 1px;">
+        <div class="col-sm-12 no-padding">
+        	<div class="col-sm-6" style="border-right:solid 1px;">
+        		<span>&nbsp;</span>
+        	</div>
+        	<div class="col-sm-6">
+        		<span>&nbsp;</span>
+        	</div>
+        </div>
+      </div>
+      <div class="col-sm-1 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1" style="border-right:solid 1px; padding:0; margin-bottom: -20px;">
+        &nbsp;
+      </div>
+    </div>
+
+    <div class="row" style="border-bottom:solid 1px;">
+      <div class="col-sm-2 box-l-grey no-padding" style="border-right:solid 1px;">
+      	<span style="font-size:8px; padding-left:20px;">GRADUATE STUDIES</span>
+      </div>
+      <div class="col-sm-2" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-3" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-2 no-padding" style="border-right:solid 1px;">
+        <div class="col-sm-12 no-padding">
+        	<div class="col-sm-6" style="border-right:solid 1px;">
+        		<span>&nbsp;</span>
+        	</div>
+        	<div class="col-sm-6">
+        		<span>&nbsp;</span>
+        	</div>
+        </div>
+      </div>
+      <div class="col-sm-1 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1" style="border-right:solid 1px; padding:0; margin-bottom: -20px;">
+        &nbsp;
+      </div>
+    </div>
+
+    <div class="row box-l-grey no-padding" style="border-bottom:solid 2px;">
+      <div class="col-sm-12 no-padding">
+        <p style="font-size: 7px; text-align: center; color:red !important; font-style: italic; -webkit-print-color-adjust: exact; margin-bottom: 1px">
+        	(Continue on a seperate sheet if necessary)
+    	</p>
+      </div>
+    </div>
+
+    <div class="row" style="border-bottom:solid 1px;">
+      <div class="col-sm-2 box-l-grey" style="border-right:solid 1px;">
+      	<span style="font-size:11px; text-align: center; padding-left: 23px;">SIGNATURE</span>
+      </div>
+      <div class="col-sm-7" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+      <div class="col-sm-1 box-l-grey no-padding" style="border-right:solid 1px;">
+        <span style="font-size:11px; text-align: center; padding-left: 18px;">DATE</span>
+      </div>
+      <div class="col-sm-2 no-padding" style="border-right:solid 1px;">
+        &nbsp;
+      </div>
+    </div>
+
   </div>
 </div>
 
-
+<div class="row">
+	<div class="col-sm-12 no-padding" style="border-right:solid 1px;">
+		<p class="pull-right" style="font-size:8px; padding-right: 20px;">CS FORM 212 (Revised 2017), Page 1 of 4</p>
+	</div>
+</div>
 
 
 
