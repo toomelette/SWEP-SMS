56918aacbde1e739f2c47aa99850e0dba91bfe65
gjg <sramisvis@gmail.com>
2021-10-21 14:18:30 +0800

    remove logsactivity from user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable{
 
 
 
-    use Notifiable, Sortable, LogsActivity, HasActivity;
+    use Notifiable, Sortable;
 
     protected $dates = ['created_at', 'updated_at', 'last_login_time'];
 
@@ -25,8 +25,6 @@ class User extends Authenticatable{
 
     protected $hidden = ['password', 'remember_token',];
 
-    protected static $logAttributes = ['*'];
-    protected static $logOnlyDirty = true;
 
 
 
