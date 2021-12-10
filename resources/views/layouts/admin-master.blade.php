bc4c5bff8acd3eb1350fea433bad44405145764e
gjg <sramisvis@gmail.com>
2021-12-10 10:40:01 +0800

    master blade fix bday

--- resources/views/layouts/admin-master.blade.php
+++ resources/views/layouts/admin-master.blade.php
@@ -25,20 +25,22 @@
 
       <div class="content-wrapper" >
 
-
-
-          @if(Hash::check(Carbon::parse(Auth::user()->employeeUnion->birthday)->format('mdy'), \Illuminate\Support\Facades\Auth::user()->password))
-          <div class="row">
-            <div class="col-md-12">
-              <a href="#" id="change_pass_href">
-              <div class="alert alert-warning alert-dismissible" style="margin: 1rem">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                <p><b>Your account is at risk. </b> It seems like you haven't changed your password yet. Please change your SWEP Account Password by clicking here.</p>
+          @if(!empty(Auth::user()->employeeUnion))
+            @if(Hash::check(Carbon::parse(Auth::user()->employeeUnion->birthday)->format('mdy'), \Illuminate\Support\Facades\Auth::user()->password))
+              <div class="row">
+                <div class="col-md-12">
+                  <a href="#" id="change_pass_href">
+                    <div class="alert alert-warning alert-dismissible" style="margin: 1rem">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                      <p><b>Your account is at risk. </b> It seems like you haven't changed your password yet. Please change your SWEP Account Password by clicking here.</p>
+                    </div>
+                  </a>
+                </div>
               </div>
-              </a>
-            </div>
-          </div>
+            @endif
           @endif
+
+
         @yield('content')
 
       </div>
