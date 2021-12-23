428f104c35e9a6133122e84b507520a8f18bcac3
gjg <sramisvis@gmail.com>
2021-12-23 09:55:28 +0800

    remove approved payment from printable dv

--- resources/views/printables/disbursement_voucher/dv_front.blade.php
+++ resources/views/printables/disbursement_voucher/dv_front.blade.php
@@ -208,7 +208,9 @@
           <div class="col-sm-6 div-height" style="border-right:solid 1.4px; padding-left: 0;"> 
             <span style="border-right: solid 1.4px; border-bottom: solid 1.4px; padding:4px; font-weight:bold;">B</span>
             <p style="margin-left:40px; margin-top:-10px; font-weight:bold;">Approved for Payment</p>
-            <p style="margin-left:200px; margin-top:-7px; font-weight:bold;">{{ number_format($disbursement_voucher->amount, 2) }}</p>
+            <p style="margin-left:200px; margin-top:-7px; font-weight:bold;">
+{{--              {{ number_format($disbursement_voucher->amount, 2) }}--}}
+            </p>
           </div>
 
         </div>
