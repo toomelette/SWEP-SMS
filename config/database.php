310bf38b4c9294399d8af4923b23ac8fa13cadc2
gjg021 <58407820+gjg021@users.noreply.github.com>
2022-03-23 12:12:51 +0800

    remove sql server database.php

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlsrv'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,17 +67,17 @@ return [
 //            'sslmode' => 'prefer',
 //        ],
 
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST_MSSQL_GASS', 'localhost'),
-            'url' => env('DATABASE_URL', 'localhost'),
-            'database' => env('DB_DATABASE_MSSQL_GASS', 'forge'),
-            'username' => env('DB_USERNAME_MSSQL_GASS', 'forge'),
-            'password' => env('DB_PASSWORD_MSSQL_GASS', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-        ],
-
+//        'sqlsrv' => [
+//            'driver' => 'sqlsrv',
+//            'host' => env('DB_HOST_MSSQL_GASS', 'localhost'),
+//            'url' => env('DATABASE_URL', 'localhost'),
+//            'database' => env('DB_DATABASE_MSSQL_GASS', 'forge'),
+//            'username' => env('DB_USERNAME_MSSQL_GASS', 'forge'),
+//            'password' => env('DB_PASSWORD_MSSQL_GASS', ''),
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//        ],
+//
 
     ],
 
