1a9485a4f5729bcf2be14771a51f701a0b6d003b
gjg021 <58407820+gjg021@users.noreply.github.com>
2022-04-05 09:37:42 +0800

    DV front and back

--- resources/views/dashboard/disbursement_voucher/print_preview.blade.php
+++ resources/views/dashboard/disbursement_voucher/print_preview.blade.php
@@ -7,14 +7,14 @@
 @section('modal-body')
     <div class="nav-tabs-custom">
         <ul class="nav nav-tabs">
-            <li class="active"><a href="#tab_1" data-toggle="tab">Front Page</a></li>
-            <li><a href="#tab_2" data-toggle="tab">Back Page</a></li>
+            <li class="active"><a href="#tab_1" data-toggle="tab">Preview (Front and back page)</a></li>
+{{--            <li><a href="#tab_2" data-toggle="tab">Back Page</a></li>--}}
         </ul>
         <div class="tab-content">
             <div class="tab-pane active" id="tab_1">
                 <div class="row">
                     <div class="col-md-6">
-                        <b>Preview:</b>
+                        <b>Please print using LETTER paper size (8.5" x 11") </b>
                     </div>
                     <div class="col-md-6">
                         <button type="button" id="print_f_btn" class="btn btn-primary pull-right btn-sm"><i class="fa fa-print"></i> Print</button>
@@ -31,22 +31,22 @@
 
             </div>
             <!-- /.tab-pane -->
-            <div class="tab-pane" id="tab_2">
-                <div class="row">
-                    <div class="col-md-6">
-                        <b>Preview:</b>
-                    </div>
-                    <div class="col-md-6">
-                        <button type="button" id="print_b_btn" class="btn btn-primary pull-right btn-sm"><i class="fa fa-print"></i> Print</button>
-                    </div>
-                </div>
-                <br>
-                <div class="bs-example">
-                    <div class="embed-responsive embed-responsive-16by9" style="height: 1019.938px;">
-                        <iframe id="print_b_frame" class="embed-responsive-item" src="{{route('dashboard.disbursement_voucher.print',[$dv->slug, "back"])}}"></iframe>
-                    </div>
-                </div>
-            </div>
+{{--            <div class="tab-pane" id="tab_2">--}}
+{{--                <div class="row">--}}
+{{--                    <div class="col-md-6">--}}
+{{--                        <b>Preview:</b>--}}
+{{--                    </div>--}}
+{{--                    <div class="col-md-6">--}}
+{{--                        <button type="button" id="print_b_btn" class="btn btn-primary pull-right btn-sm"><i class="fa fa-print"></i> Print</button>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <br>--}}
+{{--                <div class="bs-example">--}}
+{{--                    <div class="embed-responsive embed-responsive-16by9" style="height: 1019.938px;">--}}
+{{--                        <iframe id="print_b_frame" class="embed-responsive-item" src="{{route('dashboard.disbursement_voucher.print',[$dv->slug, "back"])}}"></iframe>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
 @endsection
