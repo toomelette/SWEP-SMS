d215d97c0fe0bdb7d434e7a660f92155ebbe7b19
MISVIS <sramisvis@gmail.com>
2023-06-30 18:47:52 +0800

    storage location

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,7 +45,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => '/home/swep_afd_storage/',
+            'root' => env('STORAGE_LOCATION','/home/swep_afd_storage/'),
         ],
 
         'qc' => [
